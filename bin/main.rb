c0e9feee125f2197ba1d547058dc25e28fc8196e
thneves <tsneves11@gmail.com>
2020-11-03 14:13:22 -0300

    organize events info

--- bin/main.rb
+++ bin/main.rb
@@ -5,16 +5,26 @@ require_relative '../lib/scraper_events.rb'
 
 puts 'Welcome to the World Surf League Brazil scraper'
 puts "Type '1' to check all WSL events in Brazil\nType '2' to check Brazilian surfers in the Championship Tour"
+
 mode = gets.chomp
-
-
-
-if mode == '1'
-  events = ScraperEvents.new
-  p events.brazil_events
+choices = %w[1 2]
+until choices.include?(mode)
+  puts "Digit '1' or '2'"
+  mode = gets.chomp.upcase
 end
 
-if mode == '2'
-  surfers = ScraperSurfer.new
-  p surfers.surfers_list
+game_on = true
+
+while game_on
+  if mode == '1'
+    events = ScraperEvents.new
+    puts 'Type the name of the country to check WSL events: '
+    events.scraping
+    country = gets.chomp.split.map(&:capitalize).join(' ')
+    events.brazil_events(country)
+  elsif mode == '2'
+    surfers = ScraperSurfer.new
+    system 'cls'
+    p surfers.surfers_list
+  end
 end
