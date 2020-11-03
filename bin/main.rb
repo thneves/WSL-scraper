81349fc9cab768f7103a73ff4ad65a0c6f6d039b
thneves <tsneves11@gmail.com>
2020-11-03 16:40:31 -0300

    add reset method for main

--- bin/main.rb
+++ bin/main.rb
@@ -3,28 +3,42 @@
 require_relative '../lib/scraper_surfers.rb'
 require_relative '../lib/scraper_events.rb'
 
-puts 'Welcome to the World Surf League Brazil scraper'
-puts "Type '1' to check all WSL events in Brazil\nType '2' to check Brazilian surfers in the Championship Tour"
+program_on = true
 
-mode = gets.chomp
-choices = %w[1 2]
-until choices.include?(mode)
-  puts "Digit '1' or '2'"
-  mode = gets.chomp.upcase
-end
+while program_on
 
-game_on = true
+  puts 'Welcome to the World Surf League Brazil scraper'
+  puts "Type '1' to check all WSL events by country\nType '2' to check surfers by country in the Championship Tour"
 
-while game_on
+  mode = gets.chomp
+  choices = %w[1 2]
+  until choices.include?(mode)
+    puts "Digit '1' or '2'"
+    mode = gets.chomp.upcase
+  end
+
   if mode == '1'
     events = ScraperEvents.new
-    puts 'Type the name of the country to check WSL events: '
+    puts 'Type the name of the country to check WSL events(ex: south africa): '
     events.scraping
     country = gets.chomp.split.map(&:capitalize).join(' ')
-    events.brazil_events(country)
+    puts "These are the events around the year in #{country}: \n\n"
+    events.format_info(events.brazil_events(country))
+    program_on = false
   elsif mode == '2'
     surfers = ScraperSurfer.new
     system 'cls'
     p surfers.surfers_list
   end
+
+  puts 'Do you wanna start again? Y/N'
+  reset = gets.chomp.upcase
+  choices = %w[Y N]
+  until choices.include?(reset)
+    puts "Digit 'Y' or 'N'"
+    reset = gets.chomp.upcase
+  end
+
+  program_on = true if reset == 'Y'
+  puts "Thanks for using WSL scraper!\nFor more info access: https://www.worldsurfleague.com/" if reset == 'N'
 end
