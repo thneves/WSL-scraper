5b3d20410b1781d5abcc7a360e7f297241495237
thneves <tsneves11@gmail.com>
2020-11-03 00:31:50 -0300

    add first lines for main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,20 @@
 #!/usr/bin/env ruby
 
-# require_relative '../lib/scraper.rb'
+require_relative '../lib/scraper_surfers.rb'
+require_relative '../lib/scraper_events.rb'
 
 puts 'Welcome to the World Surf League Brazil scraper'
 puts "Type '1' to check all WSL events in Brazil\nType '2' to check Brazilian surfers in the Championship Tour"
+mode = gets.chomp
+
+
+
+if mode == '1'
+  events = ScraperEvents.new
+  p events.brazil_events
+end
+
+if mode == '2'
+  surfers = ScraperSurfer.new
+  p surfers.surfers_list
+end
