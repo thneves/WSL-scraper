d86e9c2c795cf149050d2460a47e8944317d2ea7
thneves <tsneves11@gmail.com>
2020-11-05 14:15:33 -0300

    fix rubocop LineLength

--- lib/modules.rb
+++ lib/modules.rb
@@ -18,7 +18,7 @@ module Formating
   def format_events_info(info)
     message = []
     info.each do |event|
-      message.push("Date: #{event[:date]}\nEvent: #{event[:title]}\nLocation: #{event[:location]}\nTour: #{event[:tour]}\n\n")
+      message.push("#{event[:date]}\nEvent: #{event[:title]}\nTour: #{event[:tour]}\nLocation: #{event[:location]}\n\n")
     end
     message = "No events in this country or is not even a country ;)\n\n" if info.empty?
     message
