0f3c07c1b0344af2c7775c14926830d7200c071d
thneves <tsneves11@gmail.com>
2020-11-05 13:18:29 -0300

    refactor module Formating

--- lib/modules.rb
+++ lib/modules.rb
@@ -1,27 +1,18 @@
 # frozen_string_literal: true
 
-# module for methods of formating data from events or surf ranking
+# module for methods to formating data from events or surf ranking
 module Formating
   def format_surfers_info(list, filter)
-    puts "\n"
-    puts "World Championship Tour surfers from #{filter}: \n\n"
     list.each do |surfer|
       next unless surfer[:country] == filter
 
-      puts "WCT Rank: #{surfer[:ranking]}"
-      puts "Name: #{surfer[:name]}"
-      puts "More info #{surfer[:detail_info]}"
-      puts "\n"
+      puts "WCT Rank: #{surfer[:ranking]}\nName: #{surfer[:name]}\nMore info #{surfer[:detail_info]}\n\n"
     end
   end
 
   def format_events_info(info)
     info.each do |event|
-      puts "Date: #{event[:date]}"
-      puts "Event: #{event[:title]}"
-      puts "Location: #{event[:location]}"
-      puts "Tour: #{event[:tour]}"
-      puts "\n"
+      puts "Date: #{event[:date]}\nEvent: #{event[:title]}\nLocation: #{event[:location]}\nTour: #{event[:tour]}\n\n"
     end
   end
 end
